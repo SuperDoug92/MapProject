9d556f0292463c207275c71280b8345cdc633eb2
Doug <clarke.doug248@gmail.com>
2016-08-14 21:37:43 -0400

    knockout refactoring

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -15,32 +15,53 @@ ko.bindingHandlers.map = {
 };
 
 // get location, then set viewModel
-function getUserLocation(){
+function getUserLocation(callback){
   var userLocation = {};
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       userLocation = {lat:position.coords.latitude, lng:position.coords.longitude};
-      setViewModel(userLocation);
+      callback(userLocation);
     }, function() {
       // handleNoGeolocation(browserSupportFlag);
       alert("User location not available, displaying Washington, DC");
       userLocation = {lat:38.9072, lng:77.0369};
-      setViewModel(userLocation);
+      callback(userLocation);
     });
   }
   // Browser doesn't support Geolocation
   else {
     alert("User location not available, displaying Washington, DC");
     userLocation = {lat:38.9072, lng:77.0369};
-    setViewModel(userLocation);
+    callback(userLocation);
   }
-  console.log(userLocation);
   return userLocation;
+}
+
+function geocode(address){
+  var geocoder = new google.maps.Geocoder();
+  // Make sure the address isn't blank.
+  if (address == '') {
+    window.alert('You must enter an area, or address.');
+  } else {
+    geocoder.geocode(
+      {address: address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          console.log(results);
+        } else {
+          window.alert('We could not find that location - try entering a more specific place.');
+        }
+    });
+  }
+}
+
+function reverseGeocode(){
+
 }
 
 function ViewModel(location) {
   var self = this;
   self.Map = ko.observable(location);
+  self.map_location = ko.observable("Alexandria, VA");
 }
 
 function setViewModel(location){
@@ -48,4 +69,4 @@ function setViewModel(location){
   ko.applyBindings(viewModel);
 };
 
-getUserLocation();
+getUserLocation(setViewModel);
